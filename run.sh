76a911e6872bd8dbc99325f9df12520a1060aa00
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2017-09-20 11:34:51 -0700

    Forgot username in the run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nvidia-docker run --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1 -it airsim-blocks
+nvidia-docker run --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1 -it abhayagarwal/airsim-blocks
